ad02c704dd17ed39f915b0af4826ff9cefe16456
iagocarvalho <71970033+iagocarvalho07@users.noreply.github.com>
2024-08-01 20:47:43 -0300

    consumindo api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuTittleComponent } from "../../components/menu-tittle/menu-tittle.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardComponent } from "../../components/small-card/small-card.component";
 import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+import { Datum, Welcome } from '../../data/model/valoranteModel';
+import { valorantService } from '../../service/valorant.service';
+import { NgFor } from '@angular/common';
+import { AsyncPipe  } from '@angular/common';
+import { response } from 'express';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTittleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
+  imports: [MenuTittleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent, NgFor, AsyncPipe ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  AgentName: String = ""
+  agentPhoto: String = ""
+  AgentDescription: String = ""
+  ValorantAgente: Datum[] = [];
+
+
+  constructor(private ValoranteService: valorantService){}
+
+  ngOnInit(): void {
+    this.ValoranteService.getAgents().subscribe(
+      (data: Datum[]) => {
+        this.ValorantAgente = data;
+        console.log(this.ValorantAgente)
+      }
+    )
+
+
+       // console.log(this.ValorantAgente)
+
+
+  }
+
+
+
 
 }
---
+++ src/app/service/valorant.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http"
+import { Datum, Welcome } from "../data/model/valoranteModel"
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+import { response } from "express";
+
+@Injectable({providedIn: 'root'})
+export class valorantService {
+  private url = "https://valorant-api.com/v1/agents"
+
+  constructor(private httpclient: HttpClient){}
+
+  getAgents():Observable<Datum[]>{
+    return this.httpclient.get<Welcome>(this.url).pipe(
+      map(response => response.data)
+    );
+  }
+
+}
